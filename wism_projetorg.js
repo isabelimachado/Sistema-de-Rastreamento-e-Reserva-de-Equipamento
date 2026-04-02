5fa90c5334cc99d60961accf06554d19e9c5b673
isa <isabelisantos0616@gmail.com>
2026-04-02 14:47:18 -0300

    Refactor login and registration functions

--- wism_projetorg.js
+++ wism_projetorg.js
@@ -20,6 +20,16 @@ function mostrarSnack(texto, tipo = "info") {
     });
 }
 
+function IrTelaLogin(){
+    $("#login").show();
+    $("#registro").hide();
+}
+
+function IrTelaRegistra(){
+    $("#login").hide();
+    $("#registro").show();
+}
+
 $(document).ready(function () {
     //resumo: pega usuario que tiver salvo no navegador e verifica se tiver logado, se tiver esconde as telas de registro/login e aplica o estilo do body padrão das tela,agora se nao tiver, as telas ficam escondidas 
     let usuario = JSON.parse(sessionStorage.getItem("usuario"));
@@ -36,23 +46,14 @@ $(document).ready(function () {
         });
         return;
     }
+    
     $(".fundo").hide();
     $("#adm").hide();
 
-    $("#IrTelaRegistra").click(function () {
-        $("#login").hide();
-        $("#registro").show();
-    });
-
-    $("#IrTelaLogin").click(function () {
-        $("#login").show();
-        $("#registro").hide();
-    });
-
     //resumo: se tiver dado enter e não tiver cadastrando, isso em qlqr input, ele da click no botão de cadastro
     $("#nome, #setor, #emailRegistro, #senhaRegistro").on("keydown", function (e) {
-        if (e.key === "Enter" && !cadastrando) 
-        $("#fazerCadastro").trigger("click");
+        if (e.key === "Enter" && !cadastrando)
+            $("#fazerCadastro").trigger("click");
     });
     //resumo: pega valor de cada campo tirando os espaços, faz validação de campos, todo email devem terminar com  @agrosys.com.br, envia pro ajax, se vier a msg de erro vai fica falso e mostra o snack, pega as informacoes do json.session do usuario, código e se for adm, faz um ternerario no caso
     $("#fazerCadastro").on("click", function () {
@@ -81,33 +82,33 @@ $(document).ready(function () {
             "&email=" + encodeURIComponent(email) +
             "&senha=" + encodeURIComponent(senha)
         )
-        .then((data) => {
-            if (data.msg) {
-                mostrarSnack(data.msg, "erro");
+            .then((data) => {
+                if (data.msg) {
+                    mostrarSnack(data.msg, "erro");
+                    cadastrando = false;
+                    return;
+                }
+
+                sessionStorage.setItem("usuario", JSON.stringify({
+                    codigo: data.vcodigo,
+                    tipo: data.adm ? "ADM" : "USU"
+                }));
+
+                mostrarSnack("Usuário cadastrado!", "sucesso");
+
+                setTimeout(() => {
+                    location.reload();
+                }, 1000);
+            })
+            .catch((error) => {
+                console.log(error);
                 cadastrando = false;
-                return;
-            }
-
-            sessionStorage.setItem("usuario", JSON.stringify({
-                codigo: data.vcodigo,
-                tipo: data.adm ? "ADM" : "USU"
-            }));
-
-            mostrarSnack("Usuário cadastrado!", "sucesso");
-
-            setTimeout(() => {
-                location.reload(); 
-            }, 1000);
-        })
-        .catch((error) => {
-            console.log(error);
-            cadastrando = false;
-        });
+            });
     });
 
     $("#emailLogin, #senhaLogin").on("keydown", function (e) {
-        if (e.key === "Enter" && !logando) 
-        $("#fazerLogin").trigger("click");
+        if (e.key === "Enter" && !logando)
+            $("#fazerLogin").trigger("click");
     });
 
     $("#fazerLogin").on("click", function () {
@@ -127,27 +128,28 @@ $(document).ready(function () {
             "email=" + encodeURIComponent(email) +
             "&senha=" + encodeURIComponent(senha)
         )
-        .then((data) => {
-            if (!data || !data.vcodigo) {
-                mostrarSnack("E-mail ou senha inválidos!", "erro");
+            .then((data) => {
+                if (!data || !data.vcodigo) {
+                    mostrarSnack("E-mail ou senha inválidos!", "erro");
+                    logando = false;
+                    return;
+                }
+
+                sessionStorage.setItem("usuario", JSON.stringify({
+                    codigo: data.vcodigo,
+                    tipo: data.adm ? "ADM" : "USU"
+                }));
+                mostrarSnack("Login realizado!", "sucesso");
+
+                setTimeout(() => {
+                    location.reload();
+                }, 1000);
+            })
+            .catch((error) => {
+                console.error(error);
                 logando = false;
-                return;
-            }
-
-            sessionStorage.setItem("usuario", JSON.stringify({
-                codigo: data.vcodigo,
-                tipo: data.adm ? "ADM" : "USU"
-            }));
-            mostrarSnack("Login realizado!", "sucesso");
-
-            setTimeout(() => {
-                location.reload();
-            }, 1000);
-        })
-        .catch((error) => {
-            console.error(error);
-            logando = false;
-        });
+            });
     });
 
 });
+
